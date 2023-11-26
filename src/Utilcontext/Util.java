02d96f59db3fac1c52cf509bce369385bbebad1a
Huyyo <118644683+Huyyo@users.noreply.github.com>
2023-11-26 20:22:21 +0700

    HuyNQ-Commit

--- src/Utilcontext/Util.java
+++ src/Utilcontext/Util.java
@@ -44,5 +44,6 @@ public class Util {
     public static void main(String[] args) {
         System.out.println(getConnection());
         System.out.println("Hello");
+        System.out.println("Hello Huy");
     }
 }
