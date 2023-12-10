33fe70a5c1a7534fa1db7afacee19ff30cff6fc0
DoTheAnhh <dotheanh0112@gmail.com>
2023-12-10 13:57:17 +0700

    up

--- src/views/frm_Banhang.java
+++ src/views/frm_Banhang.java
@@ -15,8 +15,6 @@ import com.google.zxing.Result;
 import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
 import com.google.zxing.common.HybridBinarizer;
 import java.awt.Dimension;
-import java.awt.Event;
-import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileNotFoundException;
